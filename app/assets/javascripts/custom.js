e16a0b2d1ef451a95920b49306b6b0112cb8c5ea
Neetesh Gupta <neetesh.gupta95@gmail.com>
2020-05-07 16:54:36 +0530

    counter updated

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,6 +4,10 @@ setInterval(function(){
   if(count < 1){
     count = 10
   }
+  playTimerValue = document.getElementById('play_timer_value');
+  playTimerValue.value = count;
+  document.getElementById('play_url').value = JSON.parse(playTimerValue.getAttribute('data-req-hash'))[count];
+  document.getElementById('image').src = JSON.parse(playTimerValue.getAttribute('data-req-hash'))[count];
   document.getElementById('counter').innerHTML = count;
 
   count -= 1
